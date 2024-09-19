c5ecbc1fcb37cf9be4e9ccccf647477eb87c4fb0
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-09-19 13:03:47 +0530

    feat[discovery-server]: add the service discovery to the microservices with eureka

--- backend/like-service/src/main/java/org/bitmonsters/likeservice/LikeServiceApplication.java
+++ backend/like-service/src/main/java/org/bitmonsters/likeservice/LikeServiceApplication.java
@@ -2,6 +2,7 @@ package org.bitmonsters.likeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class LikeServiceApplication {
