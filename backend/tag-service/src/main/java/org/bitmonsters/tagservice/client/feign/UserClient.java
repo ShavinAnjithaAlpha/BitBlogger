e4ab1208a466f64c255a4d97b2f73a652ed0f014
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-09-25 22:13:43 +0530

    feat[logging]: set up the elk logging for other services as well using logstash

--- backend/tag-service/src/main/java/org/bitmonsters/tagservice/client/feign/UserClient.java
+++ backend/tag-service/src/main/java/org/bitmonsters/tagservice/client/feign/UserClient.java
@@ -1,12 +1,12 @@
 package org.bitmonsters.tagservice.client.feign;
 
-import org.bitmonsters.tagservice.client.fallback.UserClientFallback;
+import org.bitmonsters.tagservice.client.fallback.UserClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "USER-SERVICE", fallback = UserClientFallback.class)
+@FeignClient(value = "USER-SERVICE", fallbackFactory = UserClientFallbackFactory.class)
 public interface UserClient {
 
     @GetMapping("/api/v1/users/{userId}")
