abd265a56ee855e11c91f621f8db7cbb7186095c
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2024-10-02 15:24:20 +0530

    feat[tag-service]: implement circuit breaking and retry mechanism for user client in tag service

---
+++ backend/tag-service/src/main/java/org/bitmonsters/tagservice/client/config/FeignUserClientConfig.java
@@ -0,0 +1,19 @@
+package org.bitmonsters.tagservice.client.config;
+
+import feign.Retryer;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+
+public class FeignUserClientConfig {
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new ErrorDecoder.Default();
+    }
+
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(100, 1000, 5);
+    }
+
+}
