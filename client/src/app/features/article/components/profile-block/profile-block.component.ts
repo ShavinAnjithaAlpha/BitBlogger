77660e80b270fae6e59c8f7b44d7f97f228410ee
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2025-02-23 23:05:31 +0530

    client: create interface for profile cards and blocks

---
+++ client/src/app/features/article/components/profile-block/profile-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-block.component.html',
+  styleUrl: './profile-block.component.scss'
+})
+export class ProfileBlockComponent {
+
+}
