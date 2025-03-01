3d0271331b243cabfd60b5751b4d0996142fde2e
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2025-03-01 23:32:57 +0530

    client: create the navbar and configure it

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { filter } from "rxjs/operators";
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'client';
+  showNavBar = true;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.showNavBar = !['/login', '/register'].includes(event.urlAfterRedirects);
+    });
+  }
 }
